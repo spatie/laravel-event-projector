0b12b32ce62e2226a84124702f76263579de3979
freek <freek@spatie.be>
2019-04-08 13:06:52 +0200

    wip

--- src/EventHandlers/HandlesEvents.php
+++ src/EventHandlers/HandlesEvents.php
@@ -23,7 +23,7 @@ trait HandlesEvents
         $parameters = [
             'event' => $storedEvent->event,
             'storedEvent' => $storedEvent,
-            'uuid' => $storedEvent->aggregate_uuid,
+            'aggregateUuid' => $storedEvent->aggregate_uuid,
         ];
 
         if (class_exists($handlerClassOrMethod)) {
